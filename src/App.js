a45c0209622450293c15487fb912bbe32500f154
gideonlexy <gideonlexy@gmail.com>
2023-01-09 13:40:42 +0300

    Resume template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
-import Portfolio from "./Components/Portfolio";
-import dataProfile from "./resumeData";
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Header data={dataProfile} />
-        <About data={dataProfile} />
-        <Resume data={dataProfile} />
-        <Portfolio data={dataProfile} />
-        <Contact data={dataProfile} />
-        <Footer data={dataProfile} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
